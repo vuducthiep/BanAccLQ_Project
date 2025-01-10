2516690e9fc65602f5216ba268759b2b55eadc86
Vũ Đức Thiệp <vuducthiep011104@gmail.com>
2025-01-10 21:19:26 +0700

    9h 10-1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BanAccLQ_Project/',  // Thay thế "BanAccLQ_Project" bằng tên repo của bạn
 })
